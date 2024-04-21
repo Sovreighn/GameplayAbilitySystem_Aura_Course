8816d46e3caba36d63850ae0610540aa34605bbb
Sovreighn <47010887+Sovreighn@users.noreply.github.com>
2024-04-21 03:04:34 -0700

    Changes 1.3
    Created and Setup Cursor Highlight and UnHighlight on Enemies, Created a GameModeBase, Created and Setup PlayerInput including InputAction and InputMappingContext, made enemies derive from a base class for shared behaviors.

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Sovreighn Gaming
+
+
+#include "Game/AuraGameModeBase.h"
+
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
 #include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class IEnemyInterface;
+struct FInputActionValue;
+
 
 /**
  * 
@@ -13,5 +20,25 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
 	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
+
+	void CursorTrace();
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
 };
