d26c141ac51f4ef94270040a18858bcea4170d06
Sovreighn <47010887+Sovreighn@users.noreply.github.com>
2024-04-26 20:44:45 -0700

    Changes 1.6
    Created a Health and Mana globes for our UI

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Sovreighn Gaming
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Sovreighn Gaming
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
